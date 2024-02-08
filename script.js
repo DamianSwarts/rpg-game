ffac056613575af96e6b8977e9490df29954c927
Damian Swarts <damianswarts2019@gmail.com>
2024-02-08 17:28:31 +0200

    Create a function to get the monster attack value

--- script.js
+++ script.js
@@ -229,3 +229,10 @@ function attack() {
       currentWeapon--;
     }
 }
+
+// Function to get the monster attack value
+function getMonsterAttackValue(level) {
+    const hit = (level * 5) - (Math.floor(Math.random() * xp));
+    console.log(hit);
+    return hit > 0 ? hit : 0;
+}
